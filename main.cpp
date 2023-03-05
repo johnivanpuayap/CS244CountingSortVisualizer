366e29d5692e1dc77b19ccf81607a545dbfd8035
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-03-06 02:55:47 +0800

    Updated the Output and Prints

--- main.cpp
+++ main.cpp
@@ -27,8 +27,6 @@ void countingSort(vector<int>& arr, int n) {
 
     // create count array with size equal to (maxVal + 1)
     vector<int> count(maxVal + 1, 0);
-    cout << "Count array initialized to all 0's: ";
-    printArray(count, maxVal + 1, RED);
 
     // count frequency of each element in the input array
     for (int i = 0; i < n; i++) {
@@ -45,27 +43,37 @@ void countingSort(vector<int>& arr, int n) {
 
     cout << endl << "Count array after adding the sum of indexes: ";
     printArray(count, maxVal + 1, RED);
+    cout << endl;
     
     // create output array and copy elements from input array to output array in sorted order
     vector<int> output(n);
     for (int i = n-1; i >= 0; i--) {
+
+        cout << "Processing input: " << arr[i] << endl;
         output[count[arr[i]]-1] = arr[i];
+
+        cout << "\t Adding number " << arr[i] << " to position " << count[arr[i]] << " of the output array."<< endl;
         count[arr[i]]--;
-        cout << "Output array after processing " << arr[i] << ": ";
+
+        cout << "\t Decrementing the value of index " << arr[i] << endl; 
+
+        cout << "Results" << endl;
+        cout << "Output array: ";
         printArray(output, n, YELLOW);
 
-        if(i != 0) {
-            cout << "Count array after processing " << arr[i] << ": ";
-            printArray(count, maxVal + 1, RED);
-        }
+        
+        cout << "Count array: ";
+        printArray(count, maxVal + 1, RED);
         
         cout << endl;
     }
 
-    cout << "Finished Sorting!" << endl;
+    cout << "Sorting finished!" << endl;
     
     cout << "Input array: ";
     printArray(arr, n, BLUE);
+    cout << "Count array: ";
+    printArray(count, maxVal+1, RED);
     cout << "Output array: ";
     printArray(output, n, YELLOW);
 }
@@ -74,6 +82,7 @@ int main() {
     srand(time(nullptr));
 
     int n;
+    int k;
     cout << "Enter size of input array: ";
     cin >> n;
 
@@ -82,8 +91,9 @@ int main() {
     cout << "Do you want to enter the numbers manually (y/n)? ";
     cin >> choice;
     if (choice == 'y') {
-        cout << "Enter " << n << " numbers: ";
+        
         for (int i = 0; i < n; i++) {
+            cout << "Enter numbers: " << (i+1);
             cin >> arr[i];
         }
     } else {
